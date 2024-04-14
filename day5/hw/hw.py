0741563c479a3ca50eab27d9ad695dd9b8348b68
nodoyipshidze <167009988+nodoyipshidze@users.noreply.github.com>
2024-04-14 18:50:15 +0400

    Add files via upload

---
+++ day5/hw/hw.py
@@ -0,0 +1,21 @@
+my_age = 12
+mom_age = 43
+dad_age = 45
+
+print("Before adding 5 years:")
+print("My age:", my_age)
+print("Mom's age:", mom_age)
+print("Dad's age:", dad_age)
+
+
+
+my_age += 5
+mom_age += 5
+dad_age += 5
+
+
+
+print("\nAfter adding 5 years:")
+print("My age:", my_age)
+print("Mom's age:", mom_age)
+print("Dad's age:", dad_age)
