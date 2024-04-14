f550c0bdbc5e0a4bf7a8f33201e070023624cf02
nodoyipshidze <167009988+nodoyipshidze@users.noreply.github.com>
2024-04-14 18:33:03 +0400

    Add files via upload

---
+++ day1/hw.py
@@ -0,0 +1,29 @@
+from turtle import*
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+ #davamtavret otkutxedi
+
+#viwyebt karebs
+left(90)
+forward(70)
+left(90)
+forward(120)
+right(90)
+forward(50)
+right(90)
+forward(120)
+#davamtavret karebi
+
+#viwyebt saxuravs
+penup()
+goto(0,200)
+pendown()
+right(210)
+forward(140)
+right(102)
+forward(181)
