cecaa625d5b58b8cdfe4adeec2e6c60e74a270e0
nodoyipshidze <167009988+nodoyipshidze@users.noreply.github.com>
2024-04-14 18:35:10 +0400

    Add files via upload

---
+++ day6/hw/hw.py
@@ -0,0 +1,8 @@
+age = int(input("whats your age: "))
+age_in_25_years = age + 25
+print("user's age is " + str(age) + " in 25 years user's age will be" + str(age_in_25_years) + "years")
+
+
+
+
+
---
+++ day6/hw/hw6.py
@@ -0,0 +1,23 @@
+print(input("whats your name")) = "name"
+print(input("whats your surname")) = "surname"
+print(input("whats your age")) = "age"
+print( age + SURNAME + naME)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
