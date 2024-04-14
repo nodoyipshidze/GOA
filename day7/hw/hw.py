09351868215f866ded0a9a6d2da7d01dbef1f9b9
nodoyipshidze <167009988+nodoyipshidze@users.noreply.github.com>
2024-04-14 18:35:50 +0400

    Add files via upload

---
+++ day7/hw/hw.py
@@ -0,0 +1,33 @@
+user_1 = int(input("how old are you?"))
+user_2 = int(input("how old are you?"))
+user_3 = int(input("how old are you?"))
+user_4 = int(input("how old are you?"))
+user_5 = int(input("how old are you?"))
+
+mulipy_of_1_and_2 = str(user_1 * user_2)
+
+print("multiply of 1 and 2 will be " + mulipy_of_1_and_2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
